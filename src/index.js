7cdbecf23f9c44f9d7e600d60f18ae11a12831da
Charles Gallagher <cgallagher1@carthage.edu>
2019-05-03 21:33:30 -0500

    first commit

--- src/index.js
+++ src/index.js
@@ -3,6 +3,22 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import * as firebase from 'firebase';
+
+
+
+var firebaseConfig = {
+    apiKey: "AIzaSyCE8BcPrVGC5MlrrNhgFgfDfL5R2qg1Cps",
+    authDomain: "attendance-io.firebaseapp.com",
+    databaseURL: "https://attendance-io.firebaseio.com",
+    projectId: "attendance-io",
+    storageBucket: "attendance-io.appspot.com",
+    messagingSenderId: "622629013381",
+    appId: "1:622629013381:web:ed00e47b3ad1a20c"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
