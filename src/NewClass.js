6d55a49a2dc93de9b8e533aa21ded085b8bd57c6
Charles Gallagher <cgallagher1@carthage.edu>
2019-05-06 21:28:15 -0500

    Able to add Photos

--- src/NewClass.js
+++ src/NewClass.js
@@ -35,6 +35,42 @@ class NewClass extends Component {
         let StudentIDHtml = document.getElementById("StudentID").value;
         let StudentName = document.getElementById("StudentName").value;
         let StudentPicture = document.getElementById('StudentPicture').files[0];
+
+        let filename = StudentPicture.name;
+        var storageRef = firebase.storage().ref();
+        var uploadTask = storageRef.child('/studentPictures/' + filename).put(StudentPicture);
+
+        // Register three observers:
+        // 1. 'state_changed' observer, called any time the state changes
+        // 2. Error observer, called on failure
+        // 3. Completion observer, called on successful completion
+        uploadTask.on('state_changed', function (snapshot) {
+            // Observe state change events such as progress, pause, and resume
+            // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+            var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+            console.log('Upload is ' + progress + '% done');
+            switch (snapshot.state) {
+                case firebase.storage.TaskState.PAUSED: // or 'paused'
+                    console.log('Upload is paused');
+                    break;
+                case firebase.storage.TaskState.RUNNING: // or 'running'
+                    console.log('Upload is running');
+                    break;
+            }
+        }, function (error) {
+            // Handle unsuccessful uploads
+        }, function () {
+            // Handle successful uploads on complete
+            // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+            uploadTask.snapshot.ref.getDownloadURL().then(function (downloadURL) {
+                console.log('File available at', downloadURL);
+                firebase.database().ref('Classes/').child(classNameHtml + '/').child('Roster/').child(StudentIDHtml).set({
+                    StudentID: StudentIDHtml,
+                    StudentName: StudentName,
+                    StudentPicture: downloadURL
+                })
+            });
+        });
     }
 
     render() {
@@ -50,23 +86,23 @@ class NewClass extends Component {
                         </div>
                         <div id="ClassCreationDiv" className="ClassCreationDiv">
                             <h2>Class Information</h2>
-                            <label for="className">Class Name:</label>
+                            <label>Class Name:</label>
                             <input type="text" id="class_name" readOnly="readonly"></input>
                             <div className="SubmitNewStudent">
                                 <button id="EnterClass" type="button" disabled onClick={this.onClassNameSubmit}>Enter</button>
                             </div>
-                            <label for="classRoster">Add New Student:</label>
+                            <label>Add New Student:</label>
                             <div id="RoasterCreatorDiv" className="RoasterCreatorDiv">
                                 <div id="studentIDDiv" className="studentIDDiv">
                                     <label for="StudentID">Student ID:</label>
                                     <input type="text" id="StudentID"></input>
                                 </div>
                                 <div id="studentNameDiv" className="studentNameDiv">
-                                    <label for="StudentName">Student Name:</label>
+                                    <label>Student Name:</label>
                                     <input type="text" id="StudentName"></input>
                                 </div>
                                 <div id="studentPictureDiv" className="studentPictureDiv">
-                                    <label for="StudentPicture">Student Picture:</label>
+                                    <label>Student Picture:</label>
                                     <input type="file" id="StudentPicture" accept="image/*" />
                                 </div>
                                 <br></br>
@@ -74,7 +110,7 @@ class NewClass extends Component {
                                 <br></br>
                                 <br></br>
                                 <div className="SubmitNewStudent">
-                                    <button id="EnterStudent" type="button">Enter</button>
+                                    <button id="EnterStudent" type="button" onClick={this.addStudent}>Enter</button>
                                 </div>
                                 <div className="CurrentClassDiv">
                                     <label>Current Roster:</label>
